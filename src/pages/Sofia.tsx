65aa85f0f51370dae6e92d85d7c6837864fad812
Weeba-it <weebastaff@gmail.com>
2026-04-30 08:57:45 +0200

    Deleted page

--- src/pages/Sofia.tsx
+++
@@ -1,9 +0,0 @@
-const Sofia = () => (
-  <div className="h-screen flex items-center justify-center bg-black">
-    <h1 className="text-white text-[10vw] font-black text-center">
-      caccamerda
-    </h1>
-  </div>
-);
-
-export default Sofia;
